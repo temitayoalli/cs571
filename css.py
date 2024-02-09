754d3c8c1da740f85992cbcb86491a08b7ff5a99
temitayoalli <159389583+temitayoalli@users.noreply.github.com>
2024-02-08 17:17:28 -0700

    Update css.py

--- css.py
+++ css.py
@@ -1,13 +1,1 @@
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>PDF Display</title>
-</head>
-<body>
-
-    <embed src="T:\SCHOOL\SFBU Spring 2nd TRIM\CLOUD COMPUTING INFRASTRUCTURE\Assignments\week3 homework\CS571_HOMEWORK3._TIMESERVERPROJECT_TEMITAYO_ALLI_20011_SLIDES.pdf" type="application/pdf" width="100%" height="600px" />
-
-</body>
-</html>
-
